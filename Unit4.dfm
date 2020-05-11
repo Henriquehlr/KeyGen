04d04e7eac8034467ddc1b27f328563989503c4d
Your Name <you@example.com>
2020-05-11 00:57:19 -0300

    Projeto

---
+++ Unit4.dfm
@@ -0,0 +1,24 @@
+object Form4: TForm4
+  Left = 0
+  Top = 0
+  Caption = 'Neo'
+  ClientHeight = 201
+  ClientWidth = 447
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object StaticText1: TStaticText
+    Left = 216
+    Top = 72
+    Width = 23
+    Height = 17
+    Caption = 'Neo'
+    TabOrder = 0
+  end
+end
---
+++ __recovery/Unit3.pas
@@ -0,0 +1,55 @@
+unit Unit3;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, KeyGen, Unit4;
+
+type
+  TForm3 = class(TForm)
+    Label1: TLabel;
+    Button2: TButton;
+    Edit9: TEdit;
+    procedure Button2Click(Sender: TObject);
+
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm3.Button2Click(Sender: TObject);
+begin
+  begin
+    if (Edit9.Text <> form1.Edit8.Text) then
+    begin
+      ShowMessage('Chave Invalida' + #13 + #13 +
+        'Entre em Contato com o Suporte.');
+    end
+    else if (Edit9.Text = '') then
+    begin
+      ShowMessage('Chave Invalida' + #13 + #13 +
+        'Você deve preenhcer o campo validar chave.');
+    end
+    else
+    begin
+      begin
+        ShowMessage('Chave Validada Com Sucesso,' + #13 + #13 +
+          'Bem Vindo ao Neo System');
+        form4.show;
+      end
+    end
+  end;
+
+end;
+
+end.
